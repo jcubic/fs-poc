433f7562932d0dc3c1e021a2a9a6db4f500e4ecf
Jakub Jankiewicz <jcubic@onet.pl>
2024-02-04 19:22:24 +0100

    add browserFS

--- sw.js
+++ sw.js
@@ -1,28 +1,42 @@
 importScripts(
-    'https://cdn.jsdelivr.net/npm/@jcubic/wayne/index.umd.min.js',
+    'https://cdn.jsdelivr.net/npm/@jcubic/wayne/index.umd.js',
     'https://cdn.jsdelivr.net/npm/idb-keyval/dist/umd.js',
     'https://cdn.jsdelivr.net/npm/@isomorphic-git/lightning-fs@4.6.0/dist/lightning-fs.min.js',
+    'https://cdn.jsdelivr.net/npm/browserfs/dist/browserfs.js',
     'https://cdn.jsdelivr.net/gh/jcubic/static@master/js/path.js',
     'https://cdn.jsdelivr.net/gh/jcubic/static@master/js/mime.min.js'
 );
-const fs = new LightningFS('testfs');
 
-const app = new wayne.Wayne();
+const path = BrowserFS.BFSRequire('path');
+const fs = new Promise(function(resolve, reject) {
+    BrowserFS.configure({ fs: 'IndexedDB', options: {} }, function (err) {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(BrowserFS.BFSRequire('fs'));
+        }
+    });
+});
 
-const test = url => {
-    if (url.host !== self.location.hostname) {
-        return false;
-    }
-    const path = url.pathname;
-    return !path.match(/admin|sw.js/) && !path.startsWith('/content');
-};
 
-const dir = async () => {
-  const dir = await idbKeyval.get('__dir__');
-  return dir ?? '/';
-};
+fs.then(fs => {
+    const app = new wayne.Wayne();
+    const test = url => {
+        if (url.host !== self.location.hostname) {
+            return false;
+        }
+        const path = url.pathname;
+        console.log({path});
+        return !path.match(/admin|sw.js/) && !path.startsWith('/content');
+    };
 
-app.use(wayne.FileSystem({ path, fs, mime, dir, test }));
+    const dir = async () => {
+        const dir = await idbKeyval.get('__dir__');
+        return dir ?? '/';
+    };
+
+    app.use(wayne.FileSystem({ path, fs, mime, dir, test }));
+});
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(clients.claim());
